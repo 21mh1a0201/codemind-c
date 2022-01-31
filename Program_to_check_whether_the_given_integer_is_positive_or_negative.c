45f0449247a512ad5d2f0277ee76a9f8b7f558f8
21mh1a0201 <98584252+21mh1a0201@users.noreply.github.com>
2022-01-31 11:07:51 +0530

    #include<stdio.h> int main() {     double num;     scanf("%lf",&num);     if(num<=0.0)     {
        if(num==0.0)
    
        printf("you entered zero");
        else
        printf("Negative Number");
    }
    else
    printf("Positive Number");
    return 0;
    }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    double num;
+    scanf("%lf",&num);
+    if(num<=0.0)
+    {
+        
+    if(num==0.0)
+
+    printf("you entered zero");
+    else
+    printf("Negative Number");
+}
+else
+printf("Positive Number");
+return 0;
+}
